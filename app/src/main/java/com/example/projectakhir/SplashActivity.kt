3e669f64c7d68ce8a395dbe090c2abb670d7d9f6
Rannnn17 <msyukran174@gmail.com>
2020-12-09 14:53:38 +0700

    Splash anim

--- app/src/main/java/com/example/projectakhir/SplashActivity.kt
+++ app/src/main/java/com/example/projectakhir/SplashActivity.kt
@@ -4,11 +4,33 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.projectakhir.adapter.BukuAdapter
 
 class SplashActivity : AppCompatActivity() {
+
+
+    lateinit var topAnim: Animation
+    lateinit var bottomAnim: Animation
+    lateinit var image: ImageView
+    lateinit var namatoko: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation )
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation )
+
+        image = findViewById(R.id.bookstore)
+        namatoko = findViewById(R.id.namatoko)
+
+        image.animation = topAnim
+        namatoko.animation = bottomAnim
 
         Handler().postDelayed({
             val intent = Intent(this, MainActivity::class.java)
