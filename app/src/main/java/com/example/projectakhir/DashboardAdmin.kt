116f1df1a1c09e76ffdfe56b6a5de002d1923718
lanaahm <maulanamaliki2007@gmail.com>
2020-12-09 14:52:21 +0700

    login

---
+++ app/src/main/java/com/example/projectakhir/DashboardAdmin.kt
@@ -0,0 +1,11 @@
+package com.example.projectakhir
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DashboardAdmin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard_admin)
+    }
+}
