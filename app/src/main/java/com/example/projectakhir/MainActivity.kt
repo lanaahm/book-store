4513eb2a4a7e14ca510703fda8505b8b9357eb62
Rannnn17 <msyukran174@gmail.com>
2020-12-11 05:19:47 +0700

    LOGIN DAN REGIS IFX

--- app/src/main/java/com/example/projectakhir/MainActivity.kt
+++ app/src/main/java/com/example/projectakhir/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.projectakhir
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.button.MaterialButton
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_dashboard_admin.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,7 +21,10 @@ class MainActivity : AppCompatActivity() {
         var fr = supportFragmentManager.beginTransaction()
         fr.add(R.id.v_fragment, HomeFragment())
         fr.commit()
+
+
     }
+
 
     private val onBottomNavListener = BottomNavigationView.OnNavigationItemSelectedListener { i ->
         var selectFr: Fragment = HomeFragment()
--- app/src/main/java/com/example/projectakhir/UserFragment.kt
+++ app/src/main/java/com/example/projectakhir/UserFragment.kt
@@ -1,14 +1,21 @@
 package com.example.projectakhir
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_user.*
 
 class UserFragment : Fragment() {
+
+    private lateinit var auth : FirebaseAuth
 
     override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle? ): View? {
         return inflater.inflate(R.layout.fragment_user, container, false)
@@ -22,5 +29,45 @@ class UserFragment : Fragment() {
             fr?.commit()
         }
 
+        auth = FirebaseAuth.getInstance()
+
+
+        login.setOnClickListener {
+            val Emaillog = emailLog.text.toString().trim()
+            val Passwordlog = paslog.text.toString().trim()
+
+//            if (Emaillog.isEmpty()){
+//                emailLog.error = "Email harus diisi"
+//                return@setOnClickListener
+//            }
+//
+//            if (!Patterns.EMAIL_ADDRESS.matcher(Emaillog).matches()){
+//                emailLog.error = "Email tidak valid"
+//                emailLog.requestFocus()
+//                return@setOnClickListener
+//            }
+//
+//            if (Passwordlog.isEmpty() || paslog.length() < 6){
+//                paslog.error = "Password harus lebih dari 6 karakter"
+//                paslog.requestFocus()
+//                return@setOnClickListener
+//            }
+
+            loginAdmin(Emaillog, Passwordlog)
+        }
+
+
+    }
+
+    private fun loginAdmin(Emaillog: String, Passwordlog: String) {
+        auth.signInWithEmailAndPassword(Emaillog, Passwordlog)
+            .addOnCompleteListener {
+                if (it.isSuccessful){
+                    val intent = Intent(activity, DashboardAdmin::class.java)
+                    startActivity(intent)
+                }else{
+                        Toast.makeText(activity, "${it.exception?.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
